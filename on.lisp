9ad7bc205577009ae9263064c95cdcf3f4b74f9b
quinton <18607942672@163.comm>
2020-03-25 21:48:42 +0800

    some code without docing!

--- on.lisp
+++ on.lisp
@@ -1261,3 +1261,69 @@
   `(progn
      ,@(mapcar #'(lambda (p) `(propmacro ,p)
                    props))))
+
+(defun a+expand (args syms)
+  (if args
+      (let ((sym (gensym)))
+        `(let* ((,sym ,(car args))
+                (it ,sym))
+           ,(a+expand (cdr args)
+                      (append syms (list sym)))))
+      `(+ ,@syms)))
+
+(defmacro a+ (&rest args)
+  (a+expand args nil))
+
+(defun alist-expand (args syms)
+  (if args
+      (let ((sym (gensym)))
+        `(let* ((,sym ,(car args))
+                (it, sym))
+           ,(alist-expand (cdr args)
+                          (append syms (list sym)))))
+      `(list ,@syms)))
+
+(defmacro alist (&rest args)
+  (alist-expand args nil))
+
+(defun pop-symbol (sym)
+  (intern (subseq (symbol-name sym) 1)))
+
+(defun anaphex (args expr)
+  (if args
+      (let ((sym (gensym)))
+        `(let* ((,sym ,(car args))
+                (it ,sym))
+           ,(anaphex (cdr args)
+                     (append expr (list sym)))))
+      expr))
+
+(defmacro defanaph (name &optional calls)
+  (let ((calls (or calls (pop-symbol name))))
+    `(defmacro ,name (&rest args)
+       (anaphex args (list ',calls)))))
+
+(defun anaphex1 (args call)
+  (if args
+      (let ((sym (gensym)))
+        `(let* ((,sym ,(car args))
+                (it ,sym))
+           ,(anaphex1 (cdr args)
+                      (append call (list sym)))))
+      call))
+
+(defun anaphex2 (op args)
+  `(let ((it ,(car args)))
+     (,op it ,@(cdr args))))
+
+(defun anaphex3 (op args)
+  `(_f (lambda (it) (,op it ,@(cdr args))) ,(car args)))
+
+(defmacro defanaph1 (name &key calls (rule :all))
+  (let* ((opname (or calls (pop-symbol name)))
+         (body (case rule
+                 (:all `(anaphex1 args '(,opname)))
+                 (:first `(anaphex2 ',opname args))
+                 (:place `(anaphex3 ',opname args)))))
+    `(defmacro ,name (&rest args)
+       ,body)))
