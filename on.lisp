f4c17e181bda530115e82cb4c80d36125f3d03ba
unknown <abc@abc.com>
2020-03-30 05:22:11 +0800

    chapter 19 is done but no docing!

--- on.lisp
+++ on.lisp
@@ -1531,6 +1531,7 @@
   (if (simple? pat)
       (match1 `((,pat ,seq)) then else)
       (with-gensyms (gseq gelse)
+
         `(labels ((,gelse () ,else))
            ,(gen-match (cons (list gseq seq)
                              (destruc pat gseq #'simple?))
@@ -1542,3 +1543,97 @@
                  (vars-in pat #'simple?))
      (pat-match ,pat ,seq ,then ,else)))
 
+(defun make-db (&optional (size 100))
+  (make-hash-table :size size))
+
+(defvar *default-db* (make-db))
+
+(defun clear-db (&optional (db *default-db*))
+  (clrhash db))
+
+(defmacro db-query (key &optional (db '*default-db*))
+  `(gethash ,key ,db))
+
+(defun db-push (key val &optional (db *default-db*))
+  (push val (db-query key db)))
+
+(defmacro fact (pred &rest args)
+  `(progn (db-push ',pred ',args)
+          ',args))
+
+(defun lookup (pred args &optional binds)
+  (mapcan #'(lambda (x)
+              (aif2 (match x args binds) (list it)))
+          (db-query pred)))
+
+(defun interpret-not (clause binds)
+  (if (interpret-query clause binds)
+      nil
+      (list binds)))
+
+(defun interpret-or (clauses binds)
+  (mapcan #'(lambda (c)
+              (interpret-query c binds))
+          clauses))
+
+(defun interpret-and (clauses binds)
+  (if (null clauses)
+      (list binds)
+      (mapcan #'(lambda (b)
+                  (interpret-query (car clauses) b))
+              (interpret-and (cdr clauses) binds))))
+
+(defun interpret-query (expr &optional binds)
+  (case (car expr)
+    (and (interpret-and (reverse (cdr expr)) binds))
+    (or (interpret-or (cdr expr) binds))
+    (not (interpret-not (cadr expr) binds))
+    (t (lookup (car expr) (cdr expr) binds))))
+
+(defmacro with-answer (query &body body)
+  (let ((binds (gensym)))
+    `(dolist (,binds (interpret-query ',query))
+       (let ,(mapcar #'(lambda (v)
+                         `(,v (binding ',v ,binds)))
+                     (vars-in query #'atom))
+         ,@body))))
+
+(defun compile-not (q body)
+  (let ((tag (gensym)))
+    `(if (block ,tag
+           ,(compile-query q `(return-from ,tag nil))
+           t)
+         ,body)))
+
+(defun compile-or (clauses body)
+  (if (null clauses)
+      nil
+      (let ((gbod (gensym))
+            (vars (vars-in body #'simple?)))
+        `(labels ((,gbod ,vars ,body))
+           ,@(mapcar #'(lambda (cl)
+                         (compile-query cl `(,gbod ,@vars)))
+                     clauses)))))
+
+(defun compile-and (clauses body)
+  (if (null clauses)
+      body
+      (compile-query (car clauses)
+                     (compile-and (cdr clauses) body))))
+
+(defun compile-simple (q body)
+  (let ((fact (gensym)))
+    `(dolist (,fact (db-query ',(car q)))
+       (pat-match ,(cdr q) ,fact ,body nil))))
+
+(defun compile-query (q body)
+  (case (car q)
+    (and (compile-and (cdr q) body))
+    (or (compile-or (cdr q) body))
+    (not (compile-not (cadr q) body))
+    (lisp `(if ,(cadr q) ,body))
+    (t (compile-simple q body))))
+
+(defmacro with-answer (query &body body)
+  `(with-gensyms ,(vars-in query #'simple?)
+     ,(compile-query query `(progn ,@body))))
