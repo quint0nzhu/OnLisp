c1d1dfaf15c04c125f515c8a66a130a3b053d2b8
quinton <18607942672@163.comm>
2020-03-31 09:57:34 +0800

    chapter 20 is done but no docing!

--- on.lisp
+++ on.lisp
@@ -1260,7 +1260,8 @@
      ,@(mapcar #'(lambda (p) `(propmacro ,p)
                    props))))
 
-(defun a+expand (args syms);;生成其展开式，并聚集出一个这些生成符号的列表
+(defun a+expand
+    (args syms);;生成其展开式，并聚集出一个这些生成符号的列表
   (if args
       (let ((sym (gensym)))
         `(let* ((,sym ,(car args))
@@ -1637,3 +1638,58 @@
 (defmacro with-answer (query &body body);;查询编译器
   `(with-gensyms ,(vars-in query #'simple?)
      ,(compile-query query `(progn ,@body))))
+
+(defvar *actual-cont* #'values)
+
+(define-symbol-macro *cont*
+  *actual-cont*)
+
+(defmacro =lambda (parms &body body)
+  `#'(lambda (*cont* ,@parms) ,@body))
+
+(defmacro =defun (name parms &body body)
+  (let ((f (intern (concatenate 'string "=" (symbol-name name)))))
+    `(progn
+       (defmacro ,name ,parms
+         `(,' ,f *cont* ,,@parms))
+       (defun ,f (*cont* ,@parms) ,@body))))
+
+(defmacro =bind (parms expr &body body)
+  `(let ((*cont* #'(lambda ,parms ,@body))) ,expr))
+
+(defmacro =values (&rest retvals)
+  `(funcall *cont* ,@retvals))
+
+(defmacro =apply (fn &rest args)
+  `(apply ,fn *cont* ,@args))
+
+(defmacro =funcall (fn &rest args)
+  `(funcall ,fn *cont* ,@args))
+
+(defun dft (tree)
+  (cond ((null tree) nil)
+        ((atom tree) (princ tree))
+        (t (dft (car tree))
+           (dft (cdr tree)))))
+
+(defvar *saved* nil)
+
+(=defun re-start ()
+  (if *saved*
+      (funcall (pop *saved*))
+      (=values 'done)))
+
+(=defun dft-node (tree)
+  (cond ((null tree) (re-start))
+        ((atom tree) (=values tree))
+        (t (push #'(lambda () (dft-node (cdr tree)))
+                 *saved*)
+           (dft-node (car tree)))))
+
+(=defun dft2 (tree)
+  (setq *saved* nil)
+  (=bind (node) (dft-node tree)
+    (cond ((eq node 'done) (=values nil))
+          (t (princ node)
+             (re-start)))))
+
